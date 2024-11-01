abc47251d5af68a13f276c95c2b68ed4641630e8
Lennart Lemmens <lennartlemmens@gmail.com>
2024-11-01 13:08:07 +0100

    First commit.

--- Module6/m2.mjs
+++ Module6/m2.mjs
@@ -1,5 +1,6 @@
 /*
 M2: Fibonacci
+
 Schrijf code om een reeks van Fibonacci op te bouwen, je houd de reeks bij in een array. 
 Start met een array dat de waarden 0 en 1 bevat, je vult de array op tot deze 10 getallen bevat. 
 Binnen deze reeks zal elk volgend getal de som zijn van de vorige twee getallen. Het resultaat dat je moet bekomen is dus [0, 1, 1, 2, 3, 5, 8, 13, 21, 34].
